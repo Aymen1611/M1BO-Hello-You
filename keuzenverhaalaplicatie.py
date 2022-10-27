868acc4a216f5b3f81a65791b9e03e5ef0b7bc15
aymen <35670@ma-web.nl>
2022-10-27 14:16:01 +0200

    beroeps-opdracht

---
+++ keuzenverhaalaplicatie.py
@@ -0,0 +1,161 @@
+from re import A
+from sys import builtin_module_names
+from time import sleep
+
+
+print ("achraf is 9 jaar en moet vluchte uit Syrië")
+sleep (2)
+print ("hij vlucht naar nederland hij woont er nu al een paar maanden")
+sleep (2.5)
+print ("ook hij gaat naar school en maakt vrienden")
+sleep (3)
+print ("op school leerd hij verkeerde vrienden kennen ")
+sleep (1)
+print ("Ze vragem Achraf kom je met ons naar buiten naar school ?")
+sleep (2)
+print ("A= ja is goed man  ")
+print ("B= nee man ik moet naar huis ")
+buiten = input ("a of b =..")
+if buiten == ("a"):
+    print ("ik zie je om 6 uur centrum")
+    sleep (2)
+    print ("yo ashraf hoe  gaat het kom je meestelen")
+    print ("a = gaat goed man maar heb wel geld nodig  wat willen jullie stelen ")
+    print ("b = gaat goed maar ik doe daar niet aan ik ga naar huis")
+    stelen = input (" a of b?>")
+    if stelen == ("b"):
+        print ("Ashraf gaat naar huis")
+        sleep (1.5)
+        print ("ze moeder eet alleen ze vraagt ashraf waar was je ?")
+        sleep (1)
+        print ("ashraf zegt k was met vrienden buiten ")
+        sleep (2)
+        print ("ashraf gaat weer naar school en ziet ze vrienden")
+        sleep (1) 
+        print ("hij kiest ervoor om zich zelf tegaan verbeteren en niet meer met ze omgaan")
+        sleep (2)
+        print ("hij heeft nieuwe vrienden en doet zijn best op school")
+        sleep (1)
+        print ("iedereen is trots")
+
+    if stelen == ("a"):
+
+        print ("we gaan naar de bijenkorf we gaan t shirts stelen")
+        sleep (1)
+        print (" ashraf en zijn vrienden lopen naar de bijenkorf")
+        sleep (1)
+        print (" achraf is zenuwachtig en is bang dat zij  moeder erachter komt")
+        sleep (1)
+        print ("ze lopen naar binnen en stelen de tshirts ")
+        sleep (1)
+        print ("ze rennen naar buiten en het alarm gaat af de politie rijdt achter ze aan ")
+        ontsnapping = input ("wat ga je doen  a = rennen of  b = blijven staan ")
+        
+if ontsnapping == ("a"):
+    print ("je bent aan het rennen je kijkt achter je ")
+    sleep (1)
+    print (" ze hebben een van je vrienden")
+    print (" je begint spijt tekrijgen")
+    sleep (2)
+    print (" je rent met je vriend naar zijn huis")
+    sleep (3)
+    print ("je vriend zegt ik denk dat we ze kwijt zijn")
+    print ("blijf anders bij mij slapen ")
+    slapen = input ("a = ja slim b = nee ik moet naar huis me moeder is bezorgd")
+if ontsnapping == ("b"):
+    print ("je verstopt achter een auto en je word gevonden")
+    snitch =  input (" a = aan de politie vertellen b = iks")
+    if snitch == ("a"):
+       print ("je gaat naar huis en je gaat je moeder helpen met eten maken")
+       print  (" je moeder vraagt hoe het op school gaat")
+       sleep (2)
+       print ("je zegt dat alles goed was ")
+       print ("ashraf gaat weer naar school en ziet ze vrienden")
+       sleep (1) 
+       print ("hij kiest ervoor om zich zelf tegaan verbeteren en niet meer met ze omgaan")
+       sleep (2)
+       print ("hij heeft nieuwe vrienden en doet zijn best op school")
+       sleep (1)
+       print ("iedereen is trots")
+    if snitch == ("b"):
+        print (" je moet 3 dage in de cel ")
+        sleep (3)
+        print ("je gaat naar huis en je gaat je moeder helpen met eten maken")
+        print  (" je moeder vraagt hoe het op school gaat")
+        sleep (2)
+        print ("je zegt dat alles goed was ")
+        print ("ashraf gaat weer naar school en ziet ze vrienden")
+        sleep (1) 
+        print ("hij kiest ervoor om zich zelf tegaan verbeteren en niet meer met ze omgaan")
+        sleep (2)
+        print ("hij heeft nieuwe vrienden en doet zijn best op school")
+        sleep (1)
+
+if slapen == ("a"):
+    print ("ashraf je slaapt op de bank er liggen al dekens")
+    sleep (4)
+    print (" de volgende ochtend belt je moeder je wat doe je ?")
+    bel = input (" a = opnemen b = niet opnemen")
+if slapen == ("b"):
+       print ("je gaat naar huis en je gaat je moeder helpen met eten maken")
+       print  (" je moeder vraagt hoe het op school gaat")
+       sleep (2)
+       print ("je zegt dat alles goed was ")
+       print ("ashraf gaat weer naar school en ziet ze vrienden")
+       sleep (1) 
+       print ("hij kiest ervoor om zich zelf tegaan verbeteren en niet meer met ze omgaan")
+       sleep (2)
+       print ("hij heeft nieuwe vrienden en doet zijn best op school")
+       sleep (1)
+       print ("iedereen is trots")
+
+if bel == ("a"):
+    print ("hallo mama wat is er ?")
+    print (" WAT IS ER ?? WAAR BEN JE KOM NU NAAR HUIS!!")
+    sleep (2)
+    print ("je besluit om naar huis tegaan ")
+    sleep (2)
+    print ("je legt alles uit en je moeder begrijpt je")
+    sleep (2)
+    print ("ashraf gaat weer naar school en ziet ze vrienden")
+    sleep (1) 
+    print ("hij kiest ervoor om zich zelf tegaan verbeteren en niet meer met ze omgaan")
+    sleep (2)
+    print ("hij heeft nieuwe vrienden en doet zijn best op school")
+    sleep (1)
+    print ("iedereen is trots")
+if bel == ("b"):
+    print ("ashraf kom je morgen weer chillen")
+    sleep (2)
+    print (" ashraf weet niet of hij met ze vrienden wilt blijven")
+    vrienden = input ("a= met vrienden blijven b= naar huis")
+
+if vrienden == ("a"):
+    print ("ashraf bleef  met de slechte vrienden ")
+    print (" 5 maanden later")
+    sleep (2)
+    print ("ashraf is het slechte pad op gegaan hij heeft een strafblad en geen baan")
+
+if vrienden == ("b"):
+
+
+    
+    
+
+    if buiten == ("b"):
+       print (" jammer man ik zie je morgen ")
+       sleep (2)
+       print ("je gaat naar huis en je gaat je moeder helpen met eten maken")
+       print  (" je moeder vraagt hoe het op school gaat")
+       sleep (2)
+       print ("je zegt dat alles goed was ")
+       print ("ashraf gaat weer naar school en ziet ze vrienden")
+       sleep (1) 
+       print ("hij kiest ervoor om zich zelf tegaan verbeteren en niet meer met ze omgaan")
+       sleep (2)
+       print ("hij heeft nieuwe vrienden en doet zijn best op school")
+       sleep (1)
+       print ("iedereen is trots")
+
+    
+    
